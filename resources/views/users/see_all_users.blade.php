c5445862a105a83c00362ff4960771add10f9946
me <avos.mail.dev@gmail.com>
2022-04-23 21:11:23 +0200

    Initial version of see_all_users.

--- resources/views/users/see_all_users.blade.php
+++ resources/views/users/see_all_users.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container w-75">
         <!-- Message -->
+        <!-- Add an error message here. -->
         <div class="row mt-4 mb-4">
             <div class="col text-center">
                 @if ($message = Session::get('success'))
@@ -13,5 +14,46 @@
             </div>
         </div>
         <!-- /Message -->
+
+        <!-- Display all users -->
+        <!-- id, name, email, role -->
+
+        <div class="row mt-4 mb-4">
+            <!-- column -->
+            <div class="col-md-12 text-center">
+
+                <table class="table table-cover">
+
+                    <!-- table headings -->
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Role</th>
+                        </tr>
+                    </thead>
+                    <!-- /table headings -->
+
+                    <!-- table body -->
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->role }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <!-- /table body -->
+
+                </table>
+                
+            </div>
+            <!-- /column -->
+        </div>
+
+        <!-- /Display all users -->
 </div>
 @endsection
