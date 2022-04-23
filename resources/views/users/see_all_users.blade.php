bc9f5823f710ee92c595e8bf6909dfe730e05d86
me <avos.mail.dev@gmail.com>
2022-04-23 07:12:01 +0200

    Added message div to see_all_users.html

--- resources/views/users/see_all_users.blade.php
+++ resources/views/users/see_all_users.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-All users
+<div class="container w-75">
+        <!-- Message -->
+        <div class="row mt-4 mb-4">
+            <div class="col text-center">
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+                @endif
+            </div>
+        </div>
+        <!-- /Message -->
+</div>
 @endsection
