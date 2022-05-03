8171c52c5cd42964b33e4b7b3959685f4fd34bbe
me <avos.mail.dev@gmail.com>
2022-05-03 18:38:10 +0200

    alter_table_roles rm : username column.

--- database/migrations/2022_05_03_033728_alter_table_roles.php
+++ database/migrations/2022_05_03_033728_alter_table_roles.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
-            //
+            $table->dropColumn('username');
         });
     }
 
