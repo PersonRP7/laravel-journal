7bc69770ac223fed3fce66bb712541aa5609014f
me <avos.mail.dev@gmail.com>
2022-05-09 20:37:00 +0200

    roles/create now uses anonymous form component.

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -21,16 +21,7 @@
     <!-- /Alert / Errors -->
 
     <!-- Form -->
-    <div class="row mt-4 mb-4">
-        <div class="col-md-12">
-            <form action="{{ route('roles.store') }}" method="POST">
-                @csrf
-                <label for="name" class="form-label">Role</label>
-                <input type="text" name="role" class="form-control" id="role" aria-describedby="role">
-                <button type="submit" class="btn btn-outline-info mt-4">Create</button>
-            </form>
-        </div>
-    </div>
+    <x-roles.forms.create-role-anon />
     <!-- /Form -->
 
 </div>
