e389c9ebce3ee7e1d98f05f2f0854b62a4523dac
me <avos.mail.dev@gmail.com>
2022-05-10 06:19:18 +0200

    users/edit now uses admin form component. update controller method and user admin form next.

--- resources/views/components/users/forms/user-edit-form-admin.blade.php
+++ resources/views/components/users/forms/user-edit-form-admin.blade.php
@@ -1,3 +1,43 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
-</div>
+@props(['roles', 'user'])
+
+ <div class="row mt-4 mb-4">
+        <div class="col-md-12">
+            <form action="{{ route('users.update', $user->id) }}" method="POST">
+                @csrf
+                {{-- name --}}
+                <label for="name" class="form-label">Name</label>
+                <input type="text" placeholder="{{ $user->name }}" name="name" class="form-control" id="name" aria-describedby="name">
+                {{-- /name --}}
+
+                {{-- password --}}
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="password" aria-describedby="password">
+                {{-- /password --}}
+
+                {{-- password_confirmation --}}
+                <label for="password_confirmation" class="form-label">Password confirmation</label>
+                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" aria-describedby="password_confirmation">
+                {{-- /password_confirmation --}}
+
+               {{-- role select --}}
+               {{-- Check if role none --}}
+               <label for="role" class="form-label">Role</label>
+               <select name="role" id="role" class="form-control">
+                    @forelse ($roles as $role)
+                        <option value="{{ $role->role }}">{{ $role->role }}</option>
+                    @empty
+                        <option value="Set role later">Set role later</option>
+                    @endforelse
+               </select>
+               {{-- /role select --}}
+
+               {{-- email --}}
+               <label for="email">Email</label>
+               <input placeholder="{{ $user->email }}" type="email" name="email" class="form-control" id="email">
+               {{-- /email --}}
+
+
+                <button type="submit" class="btn btn-outline-info mt-4">Edit</button>
+            </form>
+        </div>
+    </div>
