5117323275455655576162e27e9c18100db7de30
me <avos.mail.dev@gmail.com>
2022-05-26 06:14:16 +0200

    Added Auth facade to PostController.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Date;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
