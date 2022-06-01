c84600b6045f44520c05c7fa73647e7405535989
me <avos.mail.dev@gmail.com>
2022-06-01 22:07:04 +0200

    journal/home_page

--- resources/views/journal/home.blade.php
+++ resources/views/journal/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-Hello World
+<div class="row text-center mt-4">
+    <div class="col">
+        <a href="{{ route('users.index') }}" class="btn btn-info">Users</a>
+    </div>
+
+    <div class="col">
+        <a href="{{ route('roles.index') }}" class="btn btn-info">Roles</a>
+    </div>
+
+    <div class="col">
+        <a href="{{ route('posts.index') }}" class="btn btn-info">Posts</a>
+    </div>
+</div>
 @endsection
