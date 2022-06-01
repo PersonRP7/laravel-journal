b3a809defb14a42ae24a6f9f3401e827ebf8646d
me <avos.mail.dev@gmail.com>
2022-06-01 22:12:48 +0200

    users/show added message if posts empty to view.

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -2,5 +2,11 @@
 
 @section('content')
 {{-- {{ $user }} --}}
-<x-posts.cards.posts-by-user :posts="$posts" />
+{{-- <x-posts.cards.posts-by-user :posts="$posts" /> --}}
+
+@if ($posts->isEmpty())
+    <p>There are no posts by this user yet.</p>
+@else
+    <x-posts.cards.posts-by-user :posts="$posts" />
+@endif
 @endsection
