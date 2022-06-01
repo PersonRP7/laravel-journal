246e844e1d25670290d5784a1463cd5efdeed086
me <avos.mail.dev@gmail.com>
2022-06-01 20:28:13 +0200

    Posts/list.blade now using cards.

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -13,14 +13,28 @@
 
     <!-- /Alert -->
 
-    @foreach ($posts as $post)
-        <p>{{ $post->title }}</p>
-        <img src="{{ $post->name }}" alt="">
-        <p>{{ $post->user->name }}</p>
-        {{-- post show --}}
-        <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
-        {{-- /post show --}}
-    @endforeach
+    <!-- Create new post if logged in -->
+
+    <!-- /Create new post if logged in -->
+
+
+    <div class="row">
+        
+        @foreach ($posts as $post)
+        {{-- <div class="col-md-3"> --}}
+            <div class="card ms-4 mt-4" style="width: 18rem;">
+            {{-- <img src="{{ $post->name }}" class="card-img-top" alt="{{ $post->name }}"> --}}
+            <a href="{{ route('posts.show', $post) }}"><img src="{{ $post->name }}" class="card-img-top" alt="{{ $post->name }}"></a>
+            <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">{{ $post->text }}</p>
+                <a href="{{ route('posts.show', $post) }}" class="btn btn-primary">Enter</a>
+            </div>
+            </div>
+        {{-- </div>     --}}
+        @endforeach
+        
+    </div>
 
 </div>
 @endsection
